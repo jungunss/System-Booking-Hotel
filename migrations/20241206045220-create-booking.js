98b407f4e859d87eea4ff84502014e85ddd02f7f
Jungun Sitompul <jungunsitompul13@gmail.com>
2024-12-06 14:19:58 +0700

    add model Booking

---
+++ migrations/20241206045220-create-booking.js
@@ -0,0 +1,64 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Bookings", {
+      order_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: true,
+        unique: true,
+      },
+      room_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        references: {
+          model: "Rooms",
+          key: "room_id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      user_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "user_id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      check_in_date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      check_out_date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      total_price: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      booking_status: {
+        type: Sequelize.ENUM("booked", "cancelled", "completed"),
+        defaultValue: "booked",
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Bookings");
+  },
+};
---
+++ models/booking.js
@@ -0,0 +1,109 @@
+"use strict";
+
+const { Model } = require("sequelize");
+const ULID = require("ulid");
+
+module.exports = (sequelize, DataTypes) => {
+  class Booking extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Booking.belongsTo(models.Room, { foreignKey: "room_id" });
+      Booking.belongsTo(models.User, { foreignKey: "user_id" });
+    }
+  }
+
+  Booking.init(
+    {
+      order_id: {
+        type: DataTypes.STRING,
+        unique: { msg: "order_id already exists" },
+        defaultValue: () => ULID.ulid(),
+        primaryKey: true,
+      },
+      room_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: { msg: "room_id is required" },
+          notEmpty: { msg: "room_id is required" },
+        },
+      },
+      user_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: { msg: "user_id is required" },
+          notEmpty: { msg: "user_id is required" },
+        },
+      },
+      check_in_date: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: { msg: "check_in_date is required" },
+          notEmpty: { msg: "check_in_date is required" },
+          isDateFormat(value) {
+            const regex = /^\d{4}-\d{2}-\d{2}$/;
+            if (!regex.test(value)) {
+              throw new Error("Invalid date format, must be YYYY-MM-DD");
+            }
+          },
+        },
+      },
+      check_out_date: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: { msg: "check_out_date is required" },
+          notEmpty: { msg: "check_out_date is required" },
+          isDateFormat(value) {
+            const regex = /^\d{4}-\d{2}-\d{2}$/;
+            if (!regex.test(value)) {
+              throw new Error("Invalid date format, must be YYYY-MM-DD");
+            }
+          },
+          checkDates() {
+            if (new Date(this.check_out_date) <= new Date(this.check_in_date)) {
+              throw new Error(
+                "check_out_date must be later than check_in_date"
+              );
+            }
+          },
+        },
+      },
+      total_price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notNull: { msg: "total_price is required" },
+          notEmpty: { msg: "total_price is required" },
+          isInt: { msg: "invalid total_price format" },
+          min: {
+            args: 50000,
+            msg: "minimum price is Rp.50,000",
+          },
+        },
+      },
+      booking_status: {
+        type: DataTypes.ENUM("booked", "cancelled", "completed"),
+        allowNull: false,
+        defaultValue: "booked",
+        validate: {
+          notNull: { msg: "booking_status is required" },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Booking",
+      timestamps: true,
+    }
+  );
+
+  return Booking;
+};
